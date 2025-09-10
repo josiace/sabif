59b4997125aa4a08e06750e9d02abf620abd9a6b
josiace <afletounoudouprince5@gmail.com>
2025-09-10 05:18:59 +0000

    Update settings.py

--- tuto/settings.py
+++ tuto/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'acceuil',
     'connexion',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
@@ -131,6 +132,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 
