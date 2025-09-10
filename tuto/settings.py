afc74b1f8ac3952710bcfb01887753c44de0b616
josiace <afletounoudouprince5@gmail.com>
2025-09-10 04:37:30 +0000

    Update settings.py

--- tuto/settings.py
+++ tuto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b2hl%fc))g^tzp)goqeb89)s%j(c1riirm)-77mgoc372$#*o-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://sabif.onrender.com']
 
 
 # Application definition
@@ -130,3 +130,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
